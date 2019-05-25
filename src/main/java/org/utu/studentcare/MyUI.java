a377dc72fba959a4a9e04d975b298a00c2bf7953
Arttu <huttunen.arttu@gmail.com>
2019-05-25 16:25:42 +0300

    WIP: First implementations of login page

--- src/main/java/org/utu/studentcare/MyUI.java
+++ src/main/java/org/utu/studentcare/MyUI.java
@@ -6,11 +6,16 @@ import com.vaadin.annotations.Theme;
 import com.vaadin.annotations.VaadinServletConfiguration;
 import com.vaadin.server.VaadinRequest;
 import com.vaadin.server.VaadinServlet;
-import com.vaadin.ui.Button;
-import com.vaadin.ui.Label;
-import com.vaadin.ui.TextField;
-import com.vaadin.ui.UI;
-import com.vaadin.ui.VerticalLayout;
+import com.vaadin.ui.*;
+import org.sqlite.SQLiteConnection;
+import org.sqlite.core.DB;
+import org.utu.studentcare.applogic.AppLogicException;
+import org.utu.studentcare.applogic.DBApp;
+import org.utu.studentcare.db.SQLConnection;
+import org.utu.studentcare.db.orm.*;
+
+import java.sql.SQLException;
+import java.util.Optional;
 
 /**
  * This UI is the application entry point. A UI may either represent a browser window 
@@ -26,22 +31,48 @@ public class MyUI extends UI {
     protected void init(VaadinRequest vaadinRequest) {
         final VerticalLayout layout = new VerticalLayout();
         
-        final TextField name = new TextField();
+
+        /* final TextField name = new TextField();
         name.setCaption("Type your name here:");
 
         Button button = new Button("Click Me");
         button.addClickListener(e -> {
-            layout.addComponent(new Label("Thanks " + name.getValue() 
+            layout.addComponent(new Label("Thanks " + name.getValue()
                     + ", it works!"));
-        });
-        
-        layout.addComponents(name, button);
-        
+        });*/
+
+
+
+
+
+        LoginForm loginForm = new LoginForm();
+        loginForm.addLoginListener(e -> {String username = e.getLoginParameter("username"); String password = e.getLoginParameter("password");});
+
+        try {
+            SQLConnection connection = SQLConnection.createConnection("value4life.db", false);
+            String authTest =  Student.authenticate(connection, loginForm.getUsernameCaption(), loginForm.getPasswordCaption()).toString();
+            layout.addComponent(new Label("DEBUG; authTest returns: " + authTest));
+            JavaScript.getCurrent().execute("window.alert('This is popup'");
+            JavaScript.getCurrent().execute("window.alert('DEBUG; authTest returns:" + authTest + "');");
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        layout.addComponents(loginForm);
+
         setContent(layout);
+
+
     }
 
     @WebServlet(urlPatterns = "/*", name = "MyUIServlet", asyncSupported = true)
     @VaadinServletConfiguration(ui = MyUI.class, productionMode = false)
     public static class MyUIServlet extends VaadinServlet {
     }
+
+    protected void initSQL() {
+
+    }
+
 }
